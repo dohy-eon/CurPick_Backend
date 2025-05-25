287a2fc1acfa764e77bd534c3d9305d8cced5eb8
HYEON <titeotty12@gmail.com>
2025-05-25 15:21:29 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/jobfeed/JobFeed/JobFeedApplication.java
@@ -0,0 +1,13 @@
+package com.jobfeed.JobFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobfeed/JobFeed/JobFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobfeed.JobFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
